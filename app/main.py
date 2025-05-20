de10777c5bec4c958daa0591e140289be66332a1
Vsevolod Morozov <morozovvsevolod15@gmail.com>
2025-05-20 21:10:21 +0100

    FixBug(uvicorn, docker) server await

--- app/main.py
+++ app/main.py
@@ -4,9 +4,11 @@ from config.config import DATABASE_URL
 import asyncio 
 from config.uvicorn_server import UvicornServer
 
+from config.logger import get_logger
+
 from fastapi import FastAPI
 
-
+logger = get_logger()
 
 async def create_app():
     app = FastAPI()
@@ -20,6 +22,17 @@ async def create_app():
 
     server = UvicornServer(app)
     await server.start()
+    
+    try:
+        while True:
+            await asyncio.sleep(1)
+    except KeyboardInterrupt:
+        logger.info('Останавливаю сервис')
+        await server.stop()
+
+
+
+
 
     
 if __name__ == "__main__":
